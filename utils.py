7f72ff922e09fecb42ccd6ea25ed05afc575d34c
Chimek <chimeksushi@gmail.com>
2021-04-05 15:43:06 +0200

    Add some features

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from random import randint
 from main import request, session
+from json import loads as json_load
 
 
 def generate_id(ids):
@@ -45,3 +46,28 @@ def request_args_to_dict(available_request_args):
             request_args[request_arg] = available_request_args[request_arg]
 
     return request_args
+
+
+def request_form_to_dict(available_request_form):
+    request_args = {}
+
+    for request_arg in available_request_form.keys():
+        if request_arg in request.args.keys():
+            request_args[request_arg] = request.form[request_arg]
+        else:
+            request_args[request_arg] = available_request_form[request_arg]
+
+    return request_args
+
+
+def check_profanity(text):
+    with open("static/profanities.json", "r") as profanities_file:
+        raw_data = profanities_file.read()
+        profanities = json_load(raw_data)
+        profanities_file.close()
+
+    for i in profanities:
+        if i in text:
+            return True
+    else:
+        return False
